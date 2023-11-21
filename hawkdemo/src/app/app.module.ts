d74279a97fefd64e36006340aec578c5413ec82b
tanay-sgl <tanay.sagrolikar@gmail.com>
2023-11-21 13:43:23 +1100

    final

--- hawkdemo/src/app/app.module.ts
+++ hawkdemo/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './list/list.component';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -37,6 +39,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     ),
     provideFirestore(() => getFirestore()),
     NoopAnimationsModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"firehawk-demo","appId":"1:212728406496:web:3ba3c32c5b2d3798374b85","storageBucket":"firehawk-demo.appspot.com","apiKey":"AIzaSyCUK5njym6GVoGD4cm0cJ0JIv3oVY5fKQ4","authDomain":"firehawk-demo.firebaseapp.com","messagingSenderId":"212728406496","measurementId":"G-7R94W0TGSE"})),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
   ],
   providers: [FirestoreService, LocalstorageService],
   bootstrap: [AppComponent],
